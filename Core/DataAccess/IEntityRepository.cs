700de79168b40be88745cceac189308db1032c7b
Mehmet Tekin ALTUN <tmehmet48.12@gmail.com>
2021-12-06 14:55:22 +0300

    new

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -10,7 +10,7 @@ namespace Core.DataAccess
 
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
-        T get(Expression<Func<T,bool>> filter);
+        T Get(Expression<Func<T,bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
